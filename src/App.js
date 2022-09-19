d1aa07cb47baa2db3aa8ef9c8bbac22e09f05927
Jaime-Ginesky <jaimelee79@gmail.com>
2022-09-18 22:01:12 -0700

    added state for ImageLink

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+       input:'',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
+  onSubmit = () => {
+    console.log('click');
+  }
   render() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}/>
       {/* <FaceRecognition />  */}
     </div>
   );
