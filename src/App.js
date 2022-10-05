05f5cca3cbd657f7433e1c7ab67dd3668e3e2e4c
Jaime-Ginesky <jaimelee79@gmail.com>
2022-10-05 14:33:59 -0700

    debugged dep err

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'eda19c663ceb4470a64ab07e61ab1cf4'
+})
 
 class App extends Component {
   constructor(){
@@ -18,6 +23,7 @@ class App extends Component {
 
   onSubmit = () => {
     console.log('click');
+    app.models.predict("eda19c663ceb4470a64ab07e61ab1cf4", "https://samples.clarifai.com/metro-north.jpg" )
   }
   render() {
   return (
