c010e3d69803fc7972a6f48dddc1525245650581
Jaime-Ginesky <jaimelee79@gmail.com>
2022-11-18 22:27:48 -0800

    integrated front and back end register

--- src/App.js
+++ src/App.js
@@ -34,11 +34,7 @@ class App extends Component {
    }
    }
 
-   componentDidMount() {
-    fetch('http//:localhost:3000')
-     .then(response => response.json())
-     .then(console.log)
-   }
+  
 
    calculateFaceLocation = (data) => {
     const clarifaiFace = JSON.parse(data, null, 2).outputs[0].data.regions[0]
